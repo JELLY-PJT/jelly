27e08bea8ac9b87e253a9ddff36b19f1fa62dbcf
SungBum Ha <gktjd425@naver.com>
2023-05-25 17:21:56 +0900

    [CREATE]apps

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'groups',
+    'schedules',
+    'diaries',
+    'chats',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
