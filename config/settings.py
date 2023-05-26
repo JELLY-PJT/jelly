3ea9090ca278d12a397ad921145a7ed8c41a965e
최수현 <sw92522@naver.com>
2023-05-26 18:00:29 +0900

    JP-4 #comment [CREATE] urls, views, serializers of group

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'schedules',
     'diaries',
     'chats',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Group
+from .serializers import UserGroupListSerializer, GroupSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def index(request):
+    serializer = UserGroupListSerializer(request.user)
+    return Response(serializer.data)
+
+
+# class GroupDetail(APIView):
+#     def get_object(self, )
+
+#     def get(self, request):
+#         pass
