ded00dd7d97ed82c10ad7626a99fa7fe7c6c00fc
최수현 <sw92522@naver.com>
2023-05-29 16:06:51 +0900

    [ADD] post DRF

--- groups/urls.py
+++ groups/urls.py
@@ -6,4 +6,6 @@ app_name = 'groups'
 urlpatterns = [
     path('', views.index, name='index'),
     # path('<int:group_pk>/', views.GroupDetail.as_view()),
+    path('<int:group_pk>/posts/', views.PostsList.as_view()),
+    path('posts/<int:post_pk>/', views.PostDetail.as_view()),
 ]
--- groups/views.py
+++ groups/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-from .models import Group
-from .serializers import UserGroupListSerializer, GroupSerializer
+from .models import Group, Post
+from .serializers import UserGroupListSerializer, PostSerializer, PostImageSerializer, PostReadSerializer
 
 
 @api_view(['GET'])
@@ -18,3 +18,65 @@ def index(request):
 
 #     def get(self, request):
 #         pass
+
+
+class PostsList(APIView):
+    def get_group(self, group_pk):
+        try:
+            return Group.objects.get(pk=group_pk)
+        except Group.DoesNotExist:
+            raise Http404
+
+    def post(self, request, group_pk):
+        group = self.get_group(group_pk)
+        new_post = PostSerializer(data=request.data)
+        success = True
+        if new_post.is_valid():
+            new_post.save(user=request.user, group=group)
+            post = Post.objects.get(pk=new_post.data['id'])
+            images = dict(request.data.lists())['image']
+            arr = []
+            for image in images:
+                image_serializer = PostImageSerializer(data={'image': image})
+                if image_serializer.is_valid():
+                    image_serializer.save(post=post)
+                    arr.append(image_serializer.data)
+                else:
+                    success = False
+        else:
+            success = False
+        if success:
+            return Response({'post': new_post.data,
+                            'images': arr,},
+                            status=status.HTTP_201_CREATED)
+        else:
+            return Response({'post': new_post.data,
+                            'images': arr,},
+                            status= status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    def get_post(self, post_pk):
+        try:
+            return Post.objects.get(pk=post_pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, post_pk):
+        post = self.get_post(post_pk)
+        # 조회수
+        if request.user not in post.hits.all():
+            post.hits.add(request.user)
+
+        serializer = PostReadSerializer(post)
+        return Response(serializer.data)
+    
+    def delete(self, request, post_pk):
+        post = self.get_post(post_pk)
+        # media file에서 image file 삭제 아직--
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+    # def put(self, request, post_pk):
+    #     post = self.get_post(post_pk)
