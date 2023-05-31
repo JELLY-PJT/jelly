18d945f25e97e22a34657c82a404dc41a65b3f0d
SungBum Ha <gktjd425@naver.com>
2023-05-31 09:58:16 +0900

    JP-5 #comment [UPDATE] diaries-create

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('diaries/', include('diaries.urls')),
     path('groups/', include('groups.urls')),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- diaries/admin.py
+++ diaries/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Diary
+from .models import Diary, DiaryComment, DiaryShare
 
 # Register your models here.
 admin.site.register(Diary)
+admin.site.register(DiaryComment)
+admin.site.register(DiaryShare)
