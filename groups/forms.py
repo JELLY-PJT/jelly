2c59f5ce2c32cbd64750150256020cf9f576b44d
LEE, SOOJUNG <119299742+ruby04-dev@users.noreply.github.com>
2023-06-01 09:49:55 +0900

    Revert "Feature/modal group create"

--- groups/forms.py
+++ groups/forms.py
@@ -1,19 +1,8 @@
 from django import forms
 from .models import Group, Post, PostImage, PostComment, Vote, VoteSelect
-from imagekit.forms import ProcessedImageField
+
 
 class GroupForm(forms.ModelForm):
-    name = forms.CharField(label='그룹 이름',  widget=forms.TextInput(
-        attrs={'class': 'create-form','style' : 'width:400px;'}))
-    thumbnail = ProcessedImageField(
-        required=True,
-        widget=forms.ClearableFileInput(attrs={'class': 'create-form','style' : 'width:400px;'}),
-        label='그룹프로필 이미지',
-        label_suffix='',
-        spec_id='image_size',
-    )
-    intro = forms.CharField(label='그룹 소개', widget=forms.Textarea(
-        attrs={'class': 'create-form','style' : 'width:400px;'}))
     class Meta:
         model = Group
         fields = ('name', 'thumbnail', 'intro',)
--- utils/context_processors.py
+++
@@ -1,5 +0,0 @@
-from groups.forms import GroupForm
-
-def get_group_form(request):
-    group_form = GroupForm()
-    return {'group_form': group_form}
