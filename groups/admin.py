acda8afeb9dc0327efc5de49041e8c8c3d319b98
최수현 <sw92522@naver.com>
2023-06-02 14:20:57 +0900

    [ADD] register models of groups to admin site

--- groups/admin.py
+++ groups/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Group
+from .models import Group, Post, PostImage, PostEmote, PostComment, Vote, VoteSelect
 
 
 admin.site.register(Group)
+admin.site.register(Post)
+admin.site.register(PostImage)
+admin.site.register(PostEmote)
+admin.site.register(PostComment)
+admin.site.register(Vote)
+admin.site.register(VoteSelect)
