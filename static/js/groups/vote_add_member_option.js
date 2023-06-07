397135f98c582a773129c7929c0fd8dbe8b78b1e
rloldl <you@example.comii319@naver.comgit config --global user.email you@example.comii319@naver.com>
2023-06-07 10:39:54 +0900

    [UPDATE] css in profile and group_detail

--- static/js/groups/vote_add_member_option.js
+++ static/js/groups/vote_add_member_option.js
@@ -1,21 +1,23 @@
 const addBtn = document.getElementById('add-member-option')
-const addOptionContainer = document.getElementById('add-option-div')
-
-addBtn.addEventListener('click', function() {
-  const optionDiv = document.createElement('div')
-  const optionInput = document.createElement('input')
-  optionInput.name = 'options'
-  // tailwind modal 가져오면 있는 class 적용
-  optionInput.classList.add('bg-gray-50', 'border', 'border-gray-300', 'text-gray-900', 'text-sm', 'rounded-lg', 'focus:ring-blue-500', 'focus:border-blue-500', 'block', 'p-2.5', 'dark:bg-gray-600', 'dark:border-gray-500', 'dark:placeholder-gray-400', 'dark:text-white')
-  const deleteBtn = document.createElement('button')
-  deleteBtn.type = 'button'
-  deleteBtn.textContent = '삭제'
-  optionDiv.appendChild(optionInput)
-  optionDiv.appendChild(deleteBtn)
-  addOptionContainer.appendChild(optionDiv)
-
-  // 삭제 버튼 클릭 시 div 삭제
-  deleteBtn.addEventListener('click', function() {
-    optionDiv.remove()
+if (addBtn) {
+  const addOptionContainer = document.getElementById('add-option-div')
+  
+  addBtn.addEventListener('click', function() {
+    const optionDiv = document.createElement('div')
+    const optionInput = document.createElement('input')
+    optionInput.name = 'options'
+    // tailwind modal 가져오면 있는 class 적용
+    optionInput.classList.add('bg-gray-50', 'border', 'border-gray-300', 'text-gray-900', 'text-sm', 'rounded-lg', 'focus:ring-blue-500', 'focus:border-blue-500', 'block', 'p-2.5', 'dark:bg-gray-600', 'dark:border-gray-500', 'dark:placeholder-gray-400', 'dark:text-white')
+    const deleteBtn = document.createElement('button')
+    deleteBtn.type = 'button'
+    deleteBtn.textContent = '삭제'
+    optionDiv.appendChild(optionInput)
+    optionDiv.appendChild(deleteBtn)
+    addOptionContainer.appendChild(optionDiv)
+  
+    // 삭제 버튼 클릭 시 div 삭제
+    deleteBtn.addEventListener('click', function() {
+      optionDiv.remove()
+    })
   })
-})
+}
