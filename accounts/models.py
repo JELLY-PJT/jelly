c72fb19c1364b8cdf171bab3fb6464f85e3b54f8
ruby04-dev <ruby04.s.lee@gmail.com>
2023-06-09 13:12:22 +0900

    [ADD] Schedules CRUD

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as BaseUserManager
 from imagekit.models import ProcessedImageField
 from imagekit.processors import Thumbnail
-from schedules.models import CustomUserManager as UserManager
 from django.contrib.contenttypes import fields
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def _create_user(self, username, email, password, **extra_fields):
+        user = super()._create_user(username, email, password, **extra_fields)
+        user.calendar.create()
+        return user
+
 
 class User(AbstractUser):
     nickname = models.CharField(max_length=20)
