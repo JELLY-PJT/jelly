b3ca067b08af2e62fbce1a1be01561ec8bd4492d
hyoung0 <choyoung1034@naver.com>
2023-06-11 15:52:24 +0900

    [UPDATE] VoteRead - 투표비동기, 투표 항목 추가 및 삭제 비동기, 프로그래스바, 스크롤바

--- static/js/groups/vote_perform.js
+++ static/js/groups/vote_perform.js
@@ -1,5 +1,6 @@
 const switchBtns = document.querySelectorAll('.switch-btn')
 
+// 투표 다시하기 버튼 클릭 시 투표 수행 Div 보이도록 함
 switchBtns.forEach(btn => {
   const voteId = btn.dataset.voteId
   const readVoteDiv = document.getElementById(`for-read-vote-${voteId}`)
@@ -13,36 +14,51 @@ switchBtns.forEach(btn => {
 const votePerformForms = document.querySelectorAll('.vote-perform-form')
 
 votePerformForms.forEach(form => {
-  const voteId = form.dataset.voteId
+  const voteId = form.dataset.voteId // 투표 pk
   const voteOverlap = form.dataset.voteOverlap // 중복투표 여부
-  const options = form.querySelectorAll('.option')
-  const selectedList = []
-  const optionsInput = document.getElementById(`${voteId}-options`)
+  const options = form.querySelectorAll('.option') // 현재 form의 선택지들
+  let selectedList = []
+  const optionsInput = document.getElementById(`${voteId}-options`) // selectedList를 담을 input tag
   options.forEach(option => {
     const optionId = option.dataset.optionId
     const userExist = option.dataset.userExist
+    const svgElement = option.querySelector('svg') // SVG 이미지 요소 선택
     if (userExist === "true") {
       // ✅여기서 선택 표시 넣어야함
+      option.classList.add('selected') // 선택된 옵션에 selected 클래스 추가
+      svgElement.classList.remove('hidden') // SVG 이미지 표시
+      option.classList.remove('blank-vote');
       selectedList.push(optionId)
       optionsInput.value = JSON.stringify(selectedList)
+
     }
     option.addEventListener('click', function() {
       // 선택했던 option을 클릭 시 제거
       if (selectedList.includes(optionId)) {
         // ✅여기서 선택 표시 없애야함
+        option.classList.remove('selected') // 선택 취소 시 selected 클래스 제거
+        svgElement.classList.add('hidden') // SVG 이미지 숨김
+        option.classList.add('blank-vote');
         const index = selectedList.findIndex(id => id === optionId)
         selectedList.splice(index, 1)
         optionsInput.value = JSON.stringify(selectedList)
       } else {
         // false인 경우 selectedList 비우기
-        if (voteOverlap === false) {
+        if (voteOverlap == "false") {
           // ✅여기서 selectedList에 있는 인자 모두 선택 표시 없애야함
+          options.forEach(opt => {
+            opt.classList.remove('selected') // 모든 옵션에서 선택 표시 제거
+            opt.querySelector('svg').classList.add('hidden') // 모든 SVG 이미지 숨김
+            opt.classList.add('blank-vote');
+          })
           selectedList = []
         }
-        if (!selectedList.includes(optionId)) {
-          // ✅여기서 선택 표시 넣어야함
-          selectedList.push(optionId)
-        }
+        // ✅여기서 선택 표시 넣어야함
+        option.classList.add('selected') // 선택된 옵션에 selected 클래스 추가
+        svgElement.classList.remove('hidden') // SVG 이미지 표시
+        option.classList.remove('blank-vote');
+
+        selectedList.push(optionId)
         optionsInput.value = JSON.stringify(selectedList)
       }
     })
@@ -51,7 +67,6 @@ votePerformForms.forEach(form => {
 
 // vote 조회수 
 const voteHitsForm = document.querySelectorAll('#vote-hits')
-console.log(voteHitsForm)
 voteHitsForm.forEach(form => {
   form.addEventListener('click', function(event) {
     event.preventDefault()
@@ -68,3 +83,15 @@ voteHitsForm.forEach(form => {
       })
   })
 })
+
+
+// progressbar
+const options = document.querySelectorAll('[id^="calculationResult-"]');
+options.forEach(function(option) {
+  let selectUsersCount = parseInt(option.getAttribute("data-select-users-count"));
+  let voteSelectCount = parseInt(option.getAttribute("data-voteselect-count"));
+  let result = (selectUsersCount / voteSelectCount) * 100;
+  result = result.toFixed(1); // 소수점 한 자리까지 제한
+  option.innerText = result + "%";
+  option.style.width = result + "%";
+});
