a0a53765526cb28a78cc4fa8a8f47a097c56ba2f
rloldl <ii319@naver.com>
2023-06-12 11:08:19 +0900

    [UPDATE] css of diary create

--- static/js/diary_create.js
+++ static/js/diary_create.js
@@ -12,28 +12,29 @@ selectThumbnailBtn.addEventListener('click', function() {
   }
 })
 
-const checkIcon = document.getElementById('check')
+const badge = document.querySelector('.thumbnail-btn')
 
 // 이미지 클릭 시 선택 처리
 function selectImageOnce() {
   if (selectedImage) {
     selectedImage.removeAttribute('id')
-    selectedImage.classList.remove('selected')
+    selectedImage.classList.remove('thumbnail-border')
+    badge.style.display = 'none'
   }
+
   this.id = 'thumbnail'
   selectedImage = this
   thumbnailInput.setAttribute('value', `${this.src}`)
   
-
-  this.classList.add('selected')
-  addBadge(this.parentElement)
+  // 썸네일 뱃지 표시
+  badge.style.display = 'block'
+  // 썸네일 뱃지 위치 조정
+  badge.style.top = this.getBoundingClientRect().top + window.scrollY + 10 + 'px'
+  badge.style.right = this.getBoundingClientRect().left - 120 + 'px'
+  // border
+  selectedImage.classList.add('thumbnail-border')
 
   for (var i = 0; i < images.length; i++) {
     images[i].removeEventListener('click', selectImageOnce)
   }
-}
-
-function addBadge(parent) {
-  parent.classList.add('selected')
-  console.log(parent)
 }
